9a1ba8474de255e39df34c4660d864e34ac56857
ozaki25 <fs65ugumori@yahoo.co.jp>
2017-05-26 07:55:42 +0900

    rootにアクセスしたら一覧にリダイレクト

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -2,11 +2,11 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class Application {
 
     public static void main(String[] args) {
@@ -14,7 +14,7 @@ public class Application {
     }
 
     @GetMapping("/")
-    public String hello() {
-        return "Hello World!";
+    public String index() {
+        return "redirect:/records";
     }
 }
