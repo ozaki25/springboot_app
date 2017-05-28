79455b2bfc997bc0aee8b5e837cffa6b85205c38
ozaki25 <fs65ugumori@yahoo.co.jp>
2017-05-29 08:31:00 +0900

    デバッグを削除

--- src/main/java/com/example/controller/RecordController.java
+++ src/main/java/com/example/controller/RecordController.java
@@ -48,7 +48,6 @@ public class RecordController {
 
     @RequestMapping(method = RequestMethod.POST)
     public String create(@ModelAttribute Record record) {
-        System.out.println(record.getDate());
         recordService.save(record);
         return "redirect:/records";
     }
